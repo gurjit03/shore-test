cc26976a6815c4ae1ae30592687eb16701e1f0f5
gurjit singh <gurjitmehta@gmail.com>
2021-08-15 12:45:12 +0530

    creates seperate folders for interfaces

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import ContactListItem from '../ContactListItem/ContactListItem'
+import {User, UserRaw} from './Contacts.Interface';
 import * as appConfig from '../../config/app';
 
 const Contacts: React.FC = (props) => {
     const [users, setUsers] = React.useState<User[]>([]);
     const fetchAndSetUsers = React.useCallback(async () => {
-        const res = await fetch("");
+        const res = await fetch(appConfig.API_URL);
         const rawUsersData = await res.json();
-        const usersData: User[] = rawUsersData.data.map(user => {
+        const usersData: User[] = rawUsersData.data.map((user: UserRaw) => {
             return {
                 id: user.id,
                 email: user.email,
@@ -15,14 +17,14 @@ const Contacts: React.FC = (props) => {
                 
             }
         })
-        setUsers(rawUsersData);
+        setUsers(usersData);
     }, []);
     React.useEffect(() => {
         fetchAndSetUsers();
     }, []);
     return (
         <div className="Contacts">
-            {users.map(user => )
+            {users.map(user => <ContactListItem key={user.id} {...user} />)}
         </div>
     )
 }   
