637d9264f7eb928239af8c446bcb4bfb1dd3ddcc
gurjit singh <gurjitmehta@gmail.com>
2021-08-15 22:01:29 +0530

    seperates out AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Provider, createStore } from './config/store';
-
-import Layout from './components/Layout/Layout'
-import ContactsPage from './pages/Contacts'
-import HomePage from './pages/Home'
-import DefaultPage from './pages/Default'
-
+import AppRouter from './Router';
 import './App.css';
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <Provider createStore={createStore}>
-        <Router>
-          <Layout>
-              <Switch>
-                <Route exact path="/">
-                  <HomePage />
-                </Route>
-                <Route exact path="/contacts">
-                  <ContactsPage />
-                </Route>
-                <Route path="*">
-                  <DefaultPage />
-                </Route>  
-              </Switch>
-          </Layout>
-        </Router>
+        <AppRouter />        
       </Provider>
     </div>
   );
